31942685448d38c9839814793cbd382e926a9836
Rzm237 <bodiosamy@gmail.com>
2025-11-28 07:36:51 +0100

    new init

---
+++ garage-sinistre/src/main/java/com/example/garagesinistre/GarageSinistreApplication.java
@@ -0,0 +1,13 @@
+package com.example.garagesinistre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageSinistreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarageSinistreApplication.class, args);
+    }
+
+}
---
+++ garage-sinistre/src/test/java/com/example/garagesinistre/GarageSinistreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.garagesinistre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageSinistreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
